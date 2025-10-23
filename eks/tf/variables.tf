85eb84f21963c0428a92b18e05f418e38161e3a9
Avi Greenbury <ialoneambest@gmail.com>
2025-10-23 09:15:50 +0100

    Driveby: remove EC2 vars from EKS TF

--- eks/tf/variables.tf
+++ eks/tf/variables.tf
@@ -31,8 +31,6 @@ variable "eks_desired_nodes" {
   default = 2
 }
 
-
-
 variable "region" {
   description = "AWS region"
   type        = string
@@ -42,41 +40,6 @@ variable "region" {
 variable "thing_name" {
   description = "Name of the EC1 Instance"
   type = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.large"
-}
-
-variable "aws_ssh_key_name" {
-  description = "Existing AWS key name"
-  type        = string
-}
-
-variable "public_ssh_key_path" {
-  description = "Path to your local SSH public key"
-  type        = string
-}
-
-variable "private_ssh_key_path" {
-  description = "Path to your local SSH private key"
-  type        = string
-}
-
-variable "ec2_ami" {
-  description = "id of AMI for EC2 VM. Should be a linux"
-  type = string
-  # amd64 debian trixie
-  default = "ami-0955d1e82085ce3e8"
-}
-
-variable "ec2_username" {
-  description = "default login username for the AMI"
-  type = string
-  # 'admin' on debian, 'ubuntu' on ubuntu
-  default = "admin"
 }
 
 variable "cx_team_name" {
